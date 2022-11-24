289910950117e6a16cdcf84d1a58948b17012688
Vishakha Sahu <vishakhasahu023@gmail.com>
2022-11-24 18:27:04 +0530

    v - 12 media query added for phone responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,31 +6,31 @@ const Footer = (props) => {
     return (
         <Fragment>
             <div className="container-F">
-                <div>
+                <div className="footer__one">
                     <img src={Logo} className="logo-home" onClick={props.onHomeClick} width="50px" height="40px" alt="logo" ></img>&nbsp;
-                    <button className="navbar-brand brand-color linkss feature__btn" onClick={props.onHomeClick}>Do.It</button><br />
+                    <button className="navbar-brand brand-color linkss feature__btn f_btn_align" onClick={props.onHomeClick}>Do.It</button><br />
                     Join millions of people who organize their work effectively with Do.It
                 </div>
                 <div>
-                    <div className="space-item"><button onClick={props.onFeatureClick} className="feature__btn footer_btn_color">Features</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">How It Works</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">For Teams</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Templates</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Refer a Friend</button> </div>
+                    <div className="space-item"><button onClick={props.onFeatureClick} className="feature__btn footer_btn_color f_btn_align">Features</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">How It Works</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">For Teams</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Templates</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Refer a Friend</button> </div>
                 </div>
                 <div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Resources</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Productivity Methods</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Integrations</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Partners</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Help Center</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Resources</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Productivity Methods</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Integrations</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Partners</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Help Center</button> </div>
                 </div>
                 <div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Company</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">About US</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">We are Hiring!</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Blog</button> </div>
-                    <div className="space-item"><button className="feature__btn footer_btn_color">Press</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Company</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">About US</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">We are Hiring!</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Blog</button> </div>
+                    <div className="space-item"><button className="feature__btn footer_btn_color f_btn_align">Press</button> </div>
                 </div>
             </div>
         </Fragment >
--- src/components/Home/HomeContentFeature.js
+++ src/components/Home/HomeContentFeature.js
@@ -12,32 +12,32 @@ const HomeContentFeature = () => {
         {
             photo: <img className="img-tran" width="150px" height="150px" src={connnectTeam} alt="team" ></img>,
             title: <p className="textTitle">Work with your Team.</p>,
-            content: <p>Connect with your work Team. Monitor Team progress closely.</p>,
+            content: <p className="home__p" >Connect with your work Team. Monitor Team progress closely.</p>,
             linkContent: <p><button className="feature__btn footer_btn_color">Connect now!</button></p>
-            
+
         },
         {
             photo: <img className="img-tran" width="150px" height="150px" src={customize} alt="custmize" ></img>,
             title: <p className="textTitle">Customize your Todo App.</p>,
-            content: <p>Add relevant filters per your requirement.</p>,
+            content: <p className="home__p">Add relevant filters per your requirement.</p>,
             linkContent: <p><button className="feature__btn footer_btn_color">Customize now!</button></p>
         },
         {
             photo: <img className="img-tran" width="150px" height="150px" src={Family} alt="family" ></img>,
             title: <p className="textTitle">Don't miss out on Family.</p>,
-            content: <p>Complete your Work efficiently and take out Family Time. Share your Calendar.</p>,
+            content: <p className="home__p">Complete your Work efficiently and take out Family Time. Share your Calendar.</p>,
             linkContent: <p><button className="feature__btn footer_btn_color">Keep your Family in Loop!</button></p>
         },
         {
             photo: <img className="img-tran" width="150px" height="150px" src={productivity} alt="productivity" ></img>,
             title: <p className="textTitle">Increase your Productivity.</p>,
-            content: <p>Focus on more important Tasks thereby increasing your productivity.</p>,
+            content: <p className="home__p">Focus on more important Tasks thereby increasing your productivity.</p>,
             linkContent: <p><button className="feature__btn footer_btn_color">Control your Priority!</button></p>
         },
         {
             photo: <img className="img-tran" width="150px" height="150px" src={template} alt="templates" ></img>,
             title: <p className="textTitle">Crazy Templates.</p>,
-            content: <p>Go Crazy with the amount of Customization on top of Templates.</p>,
+            content: <p className="home__p">Go Crazy with the amount of Customization on top of Templates.</p>,
             linkContent: <p><button className="feature__btn footer_btn_color">Check out Templates!</button></p>
         }
 
